e046245d6c866c7f9cf51b66ee077de02b867909
NIKHIL SINGH <niks604@gmail.com>
2023-04-09 20:01:07 +0530

    final

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// getting elements from indexedDB.html
 var input=document.querySelector("#input");
 var output=document.querySelector("#output");
 var btnSubmit=document.querySelector("#btn-submit");
@@ -6,9 +5,23 @@ var btnSubmit=document.querySelector("#btn-submit");
 
 var url="https://api.funtranslations.com/translate/minion.json"
 
-btnSubmit.addEventListener("click", eventTranslate)
+
+btnSubmit.addEventListener("click", eventTranslate);
 
 function eventTranslate() {
     var inputText=input.value;
-    output.innerText= "TRANSLATED "+ inputText
+    var newURL= constructURL(inputText);
+    console.log("new URL is "+ newURL);
+
+    fetch(newURL)
+    .then(response => response.json())
+    .then(json => {
+        output.innerText = json.contents.translated;
+    })
+    .catch(()=> alert("something went wrong! try after sometime."))
+}
+
+function constructURL(inputText){
+var encodedURI= encodeURI(inputText);
+return `${url}?text=${encodedURI}`;
 }
