6610fcde9837bc50bc4356cef5d13556cde93ada
NIKHIL SINGH <niks604@gmail.com>
2023-04-09 20:51:02 +0530

    completed

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 var input=document.querySelector("#input");
 var output=document.querySelector("#output");
 var btnSubmit=document.querySelector("#btn-submit");
+var btnReset=document.querySelector("#btn-reset");
 
 
 var url="https://api.funtranslations.com/translate/minion.json"
 
 
 btnSubmit.addEventListener("click", eventTranslate);
+btnReset.addEventListener("click", clear);
+
 
 function eventTranslate() {
+    console.log("TRANSLATE CLICKED")
     var inputText=input.value;
     var newURL= constructURL(inputText);
     console.log("new URL is "+ newURL);
@@ -21,7 +25,15 @@ function eventTranslate() {
     .catch(()=> alert("something went wrong! try after sometime."))
 }
 
+
 function constructURL(inputText){
 var encodedURI= encodeURI(inputText);
 return `${url}?text=${encodedURI}`;
+}
+
+
+function clear(){
+    console.log("RESET CLICKED")
+    input.value="";
+    output.value="";
 }
